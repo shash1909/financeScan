b2e83becdf4e91f3d7242fafd3c3769be38b7134
shash1909 <150889860+shash1909@users.noreply.github.com>
2025-12-01 22:56:48 +0530

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -41,7 +41,7 @@ const HeroSection = () => {
             <Button size="lg" className="px-8">
               Get Started
             </Button>
-
+          </Link>
         </div>
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
